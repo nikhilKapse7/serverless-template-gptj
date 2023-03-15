a3118fd5aaa4b5eeecb62a496f21b6669bfdb452
Nikhil Kapse <nikhilkapse77@gmail.com>
2023-03-14 22:07:20 -0700

    Added parameters

--- app.py
+++ app.py
@@ -30,6 +30,13 @@ def inference(model_inputs:dict) -> dict:
 
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
+    num_return_sequences = model_inputs.get('num_return_sequences', 1)
+    max_length = model_inputs.get('max_length', 50)
+    temperature = model_inputs.get('temperature', 1.0)
+    top_k = model_inputs.get('top_k', 50)
+    top_p = model_inputs.get('top_p', 1.0)
+    repetition_penalty = model_inputs.get('repetition_penalty', 1.0)
+    return_full_text = model_inputs.get('return_full_text', False)
     if prompt == None:
         return {'message': "No prompt provided"}
     
@@ -37,7 +44,7 @@ def inference(model_inputs:dict) -> dict:
     input_tokens = tokenizer.encode(prompt, return_tensors="pt").to(device)
 
     # Run the model
-    output = model.generate(input_tokens)
+    output = model.generate(input_tokens, max_length=max_length, temperature=temperature, top_k=top_k, top_p=top_p, repetition_penalty=repetition_penalty, do_sample=True, num_return_sequences=num_return_sequences, return_full_text=return_full_text)
 
     # Decode output tokens
     output_text = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
